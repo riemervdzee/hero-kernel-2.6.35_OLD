d45e6e42212bb33977bbfc50c9112515259c3bd1
riemervdzee <riemervdzee@gmail.com>
2011-01-02 23:15:16 +0100

    Let the battery behave more like the older hero-sources. Still got an anoying lag though

--- include/linux/ds2784_battery.h
+++ include/linux/ds2784_battery.h
@@ -1,7 +1,5 @@
-/* include/linux/ds2784_battery.h
- *
- * Copyright (C) 2009 HTC Corporation
- * Copyright (C) 2009 Google, Inc.
+/*
+ * Copyright (C) 2007 HTC Incorporated
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -11,19 +9,37 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
  */
+#ifndef _DS2784_BATTERY_H_
+#define _DS2784_BATTERY_H_
+#include <linux/notifier.h>
+#include <mach/htc_battery.h>
 
-#ifndef __LINUX_DS2784_BATTERY_H
-#define __LINUX_DS2784_BATTERY_H
-
-#ifdef __KERNEL__
-
-struct ds2784_platform_data {
-	int (*charge)(int on, int fast);
-	void *w1_slave;
+enum ds2784_notify_evt_t{
+	DS2784_CHARGING_CONTROL = 0,
+	DS2784_LEVEL_UPDATE,
+	DS2784_BATTERY_FAULT,
+	DS2784_OVER_TEMP,
+	DS2784_NUM_EVENTS,
 };
 
-#endif /* __KERNEL__ */
+#ifdef CONFIG_BATTERY_DS2784
+extern int ds2784_register_notifier(struct notifier_block *nb);
+extern int ds2784_unregister_notifier(struct notifier_block *nb);
+extern int ds2784_get_battery_info(struct battery_info_reply *batt_info);
+extern ssize_t htc_battery_show_attr(struct device_attribute *attr,
+			char *buf);
+#else
+static int ds2784_register_notifier(struct notifier_block *nb) { return 0; }
+static int ds2784_unregister_notifier(struct notifier_block *nb) { return 0; }
+static int ds2784_get_battery_info(struct battery_info_reply *batt_info)
+{
+	batt_info->level = 10;
+	return 0;
+}
+extern ssize_t htc_battery_show_attr(struct device_attribute *attr,
+			char *buf){ return 0; }
 
-#endif /* __LINUX_DS2784_BATTERY_H */
+#endif
+
+#endif
