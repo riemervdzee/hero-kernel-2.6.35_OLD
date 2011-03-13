63f2c9c90b7c3fccc75df10b119b0f2f7e71cbce
riemervdzee <riemervdzee@gmail.com>
2011-03-13 16:08:12 +0100

    Adds "msm_i2c_gpio_init" function from legend.32 sources. Ensures gpios are requested

--- arch/arm/mach-msm/include/mach/system.h
+++ arch/arm/mach-msm/include/mach/system.h
@@ -13,6 +13,9 @@
  *
  */
 
+#ifndef __ASM_ARCH_MSM_SYSTEM_H
+#define __ASM_ARCH_MSM_SYSTEM_H
+
 #include <mach/hardware.h>
 
 void arch_idle(void);
@@ -27,5 +30,7 @@ static inline void arch_reset(char mode, const char *cmd)
  */
 extern void (*msm_hw_reset_hook)(void);
 
-/*void msm_set_i2c_mux(bool gpio, int *gpio_clk, int *gpio_dat);*/
 void msm_set_i2c_mux(bool gpio, int *gpio_clk, int *gpio_dat, int clk_str, int dat_str);
+void msm_i2c_gpio_init(void);
+
+#endif
